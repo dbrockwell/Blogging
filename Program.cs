a7063a36bcfba8220df0e1d35092b1405a576451
David Brockwell <dbrockwell@my.wctc.edu>
2022-04-03 00:35:49 -0500

    added do while loop

--- Program.cs
+++ Program.cs
@@ -13,52 +13,56 @@ namespace BlogsConsole
         {
             logger.Info("Program started");
 
-            Console.WriteLine("1. Display All Blogs");
-            Console.WriteLine("2. Add Blog");
-            Console.WriteLine("3. Create Post");
-            Console.WriteLine("4. Display Posts");
-            string choose = Console.ReadLine();
+            string choose = "";
 
-            if (choose == "1") {
-                try
-                {
-                    var db = new BloggingContext();
-                    var query = db.Blogs.OrderBy(b => b.Name);
-                    Console.WriteLine("All blogs in the database:");
-                    foreach (var item in query)
+            do{
+                Console.WriteLine("1. Display All Blogs");
+                Console.WriteLine("2. Add Blog");
+                Console.WriteLine("3. Create Post");
+                Console.WriteLine("4. Display Posts");
+                choose = Console.ReadLine();
+
+                if (choose == "1") {
+                    try
                     {
-                        Console.WriteLine(item.Name);
+                        var db = new BloggingContext();
+                        var query = db.Blogs.OrderBy(b => b.Name);
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
                     }
                 }
-                catch (Exception ex)
-                {
-                    logger.Error(ex.Message);
+
+                if (choose == "2") {
+                    try
+                    {
+
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+
+                        var blog = new Blog { Name = name };
+
+                        var db = new BloggingContext();
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+
+                        // Display all Blogs from the database
+                        var query = db.Blogs.OrderBy(b => b.Name);
+
+                    }
+                        catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
+                    }
                 }
-            }
-
-            if (choose == "2") {
-                try
-                {
-
-                    // Create and save a new Blog
-                    Console.Write("Enter a name for a new Blog: ");
-                    var name = Console.ReadLine();
-
-                    var blog = new Blog { Name = name };
-
-                    var db = new BloggingContext();
-                    db.AddBlog(blog);
-                    logger.Info("Blog added - {name}", name);
-
-                    // Display all Blogs from the database
-                    var query = db.Blogs.OrderBy(b => b.Name);
-
-                }
-                    catch (Exception ex)
-                {
-                    logger.Error(ex.Message);
-                }
-            }
+            } while (choose == "1" || choose == "2" || choose == "3" || choose == "4");
 
             logger.Info("Program ended");
         }
