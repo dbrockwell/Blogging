7b765c86afa9713fd05866c2b9763295ebb81716
David Brockwell <dbrockwell@my.wctc.edu>
2022-04-03 00:28:41 -0500

    created menu and split existing content

--- Program.cs
+++ Program.cs
@@ -13,31 +13,51 @@ namespace BlogsConsole
         {
             logger.Info("Program started");
 
-            try
-            {
+            Console.WriteLine("1. Display All Blogs");
+            Console.WriteLine("2. Add Blog");
+            Console.WriteLine("3. Create Post");
+            Console.WriteLine("4. Display Posts");
+            string choose = Console.ReadLine();
 
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+            if (choose == "1") {
+                try
                 {
-                    Console.WriteLine(item.Name);
+
+                    // Create and save a new Blog
+                    Console.Write("Enter a name for a new Blog: ");
+                    var name = Console.ReadLine();
+
+                    var blog = new Blog { Name = name };
+
+                    var db = new BloggingContext();
+                    db.AddBlog(blog);
+                    logger.Info("Blog added - {name}", name);
+
+                    // Display all Blogs from the database
+                    var query = db.Blogs.OrderBy(b => b.Name);
+
+                }
+                    catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
                 }
             }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
+
+            if (choose == "2") {
+                try
+                {
+                    var db = new BloggingContext();
+                    var query = db.Blogs.OrderBy(b => b.Name);
+                    Console.WriteLine("All blogs in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
+                }
             }
 
             logger.Info("Program ended");
